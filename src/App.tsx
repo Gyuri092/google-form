87bc9f5d75b69cabbcde3872f5567d82bdabf5c0
GyuriGwon <ggr00000@gmail.com>
2023-10-04 23:35:30 +0900

    init: React-router-dom 설치 및 라우팅 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import { SurveyPreview } from './routes/SurveyPreview';
+import { SurveyEditor } from './routes/SurveyEditor';
+
 function App() {
-  return <div>app</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SurveyEditor />}>
+        <Route path="preview" element={<SurveyPreview />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
