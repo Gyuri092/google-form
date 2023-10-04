aa27df4a343df0fe082a03905d9fa1c15e654ca0
GyuriGwon <ggr00000@gmail.com>
2023-10-04 21:24:28 +0900

    init: esLint 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,49 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, node: true },
+  ignorePatterns: ['node_modules', 'dist', 'build', 'public', '.eslintrc.cjs'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint', 'simple-import-sort'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
+    // import
+    // .js, .jsx, .ts, .tsx는 임포트 시 확장자 생략
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        ignorePackages: true,
+        pattern: {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      },
+    ],
+    // console.log 찾기 쉽게 경고로 표시
+    'no-console': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: '**/tsconfig.json',
+      },
+    },
   },
   root: true,
 };
