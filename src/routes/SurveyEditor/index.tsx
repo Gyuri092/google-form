f7f1a9ac642440f7d32e39bd418e418c466ca5ae
GyuriGwon <ggr00000@gmail.com>
2023-10-05 12:25:24 +0900

    style: 콘텐츠 영역 나누기

--- src/routes/SurveyEditor/index.tsx
+++ src/routes/SurveyEditor/index.tsx
@@ -1,3 +1,38 @@
+import { css } from '@emotion/react';
+
 export const SurveyEditor = () => {
-  return <div>survey-Editor</div>;
+  return (
+    <div
+      css={css`
+        display: flex;
+        justify-content: center;
+      `}
+    >
+      <div
+        css={css`
+          width: 50%;
+          display: flex;
+          flex-direction: column;
+          border: 1px solid black;
+        `}
+      >
+        <div
+          css={css`
+            height: 50%;
+            border: 1px solid green;
+          `}
+        >
+          title+subscripttion
+        </div>
+        <div
+          css={css`
+            height: 50%;
+            border: 1px solid red;
+          `}
+        >
+          questions
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/style/index.ts
+++ src/style/index.ts
@@ -2,6 +2,8 @@ import { css } from '@emotion/react';
 
 const reset = css`
   * {
+    width: 100%;
+    height: 100%;
     margin: 0;
     padding: 0;
     border: 0;
