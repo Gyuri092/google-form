8356431a47fca21fc715a172f9dc07dc7002d9c3
GyuriGwon <ggr00000@gmail.com>
2023-10-08 00:37:28 +0900

    feat: Select 컴포넌트로 분리

--- src/components/QuestionArea/index.tsx
+++ src/components/QuestionArea/index.tsx
@@ -1,13 +1,14 @@
 import { css } from '@emotion/react';
 import { useState } from 'react';
 import { Input } from '../Input';
+import { Select } from '../Select';
 
 export const QuestionArea = () => {
   const [isFocused, setIsFocused] = useState(false);
   return (
     <form
       css={css`
-        height: 50%;
+        height: ${isFocused ? '323px' : '176px'};
         border: 1px solid #dadce0;
         border-radius: 8px;
         padding: 20px;
@@ -16,7 +17,7 @@ export const QuestionArea = () => {
       onFocus={() => setIsFocused(true)}
       onBlur={() => setIsFocused(false)}
     >
-      {isFocused && (
+      {isFocused ? (
         <div
           css={css`
             position: absolute;
@@ -27,29 +28,43 @@ export const QuestionArea = () => {
             border-radius: 8px 0 0 8px;
           `}
         />
+      ) : (
+        <div
+          css={css`
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 6px;
+            background: #137333;
+            border-radius: 8px 0 0 8px;
+          `}
+        />
       )}
       <div
         css={css`
           display: flex;
+          justify-content: space-between;
         `}
       >
-        <Input />
-        <select
-          name="question-option"
-          id=""
+        <div
           css={css`
-            width: 208px;
-            height: 48px;
-            border-radius: 4px solid #dadce0;
-            outline: none;
+            width: 446px;
           `}
         >
-          <option value="short-answer">단답형</option>
-          <option value="long-sentence">장문형</option>
-          <option value="multiple-choice-questions">객관식 질문</option>
-          <option value="check-box">체크 박스</option>
-          <option value="drop-down">드롭 다운</option>
-        </select>
+          <Input
+            inputStyle={css`
+              font-size: 11pt;
+              padding: 16px;
+              &:focus {
+                background-color: #f1f3f4;
+                &:hover {
+                  background-color: #0000001a;
+                }
+              }
+            `}
+          />
+        </div>
+        <Select />
       </div>
     </form>
   );
---
+++ src/components/Select/index.tsx
@@ -0,0 +1,27 @@
+import { css } from '@emotion/react';
+
+export const Select = () => {
+  return (
+    <select
+      name="question-option"
+      id=""
+      css={css`
+        width: 208px;
+        height: 48px;
+        border-radius: 4px;
+        border: 1px solid #dadce0;
+        text-align: center;
+        padding: 8px 48px;
+        outline: none;
+      `}
+    >
+      <option value="short-answer">단답형</option>
+      <option value="long-sentence">장문형</option>
+      <option value="multiple-choice-questions" selected>
+        객관식 질문
+      </option>
+      <option value="check-box">체크 박스</option>
+      <option value="drop-down">드롭 다운</option>
+    </select>
+  );
+};
