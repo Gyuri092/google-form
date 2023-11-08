ff6aa17f314063e3ba200c5f0aaadc060269d54a
GyuriGwon <ggr00000@gmail.com>
2023-11-08 16:21:57 +0900

    chore: 단답형/장문형 질문 컴포넌트 div -> input 태그로 수정

--- src/components/QuestionOption/WritingAnswer/index.tsx
+++ src/components/QuestionOption/WritingAnswer/index.tsx
@@ -2,23 +2,26 @@ import { css } from '@emotion/react';
 
 export const WritingAnswer = (props: { questionType: string }) => {
   return (
-    <div
+    <input
       css={css`
         width: ${props.questionType === 'long-sentence' ? '85%' : '50%'};
         height: 30px;
         padding: 1px 1px 1px 0;
-        margin-bottom: 50px;
+        margin-bottom: 24px;
         border-bottom: 1px dotted #70757a;
         font-size: 14px;
         font-weight: 400;
         letter-spacing: 0.2px;
         line-height: 20px;
         color: #70757a;
+        background: #fff;
       `}
-    >
-      {props.questionType === 'long-sentence'
-        ? '장문형 텍스트'
-        : '단답형 텍스트'}
-    </div>
+      disabled
+      placeholder={
+        props.questionType === 'long-sentence'
+          ? '장문형 텍스트'
+          : '단답형 텍스트'
+      }
+    />
   );
 };
