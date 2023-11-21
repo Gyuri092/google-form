979c210632278e2be88577547c08611b51f251a6
GyuriGwon <ggr00000@gmail.com>
2023-11-21 12:32:35 +0900

    feat: preview 용 타이틀영역, 답변영역 생성

---
+++ src/components/AnswerTitleArea/index.tsx
@@ -0,0 +1,75 @@
+import { css } from '@emotion/react';
+import { useState } from 'react';
+import { Input } from '../Input';
+
+export const AnswerTitleArea = () => {
+  const [isFocused, setIsFocused] = useState(false);
+  return (
+    <div
+      css={css`
+        height: auto;
+        position: relative;
+        margin-bottom: 14px;
+        background: #fff;
+        border-radius: 8px;
+      `}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+    >
+      <div
+        css={css`
+          position: absolute;
+          width: 100%;
+          height: 10px;
+          background: #673ab6;
+          border-radius: 8px 8px 0 0;
+          z-index: 1;
+        `}
+      />
+      <form
+        css={css`
+          width: 100%;
+          border: 1px solid green;
+          border: 1px solid #dadce0;
+          border-radius: 8px;
+          box-shadow: ${isFocused && '0 3px 3px rgba(0, 0, 0, 0.12)'},
+            ${isFocused && '0 1px 1px rgba(0, 0, 0, 0.14)'},
+            ${isFocused && '0 1px 2px rgba(0, 0, 0, 0.2)'};
+          padding: 30px 20px;
+          position: relative;
+          input {
+            background: #fff;
+            color: #000;
+            border: none;
+          }
+        `}
+      >
+        {isFocused && (
+          <div
+            css={css`
+              position: absolute;
+              left: -1px;
+              top: 1px;
+              width: 6px;
+              background: #4285f4;
+              border-radius: 8px 0 0 8px;
+            `}
+          />
+        )}
+        <Input
+          disabled
+          placeholder="설문지 제목"
+          value={localStorage.getItem('title') ?? ''}
+        />
+        <Input
+          disabled
+          placeholder="설문지 설명"
+          inputStyle={css`
+            font-size: 11pt;
+          `}
+          value={localStorage.getItem('description') ?? ''}
+        />
+      </form>
+    </div>
+  );
+};
--- src/routes/SurveyPreview/index.tsx
+++ src/routes/SurveyPreview/index.tsx
@@ -1,3 +1,30 @@
+import { css } from '@emotion/react';
+import { AnswerArea } from '../../components/AnswerArea';
+import { AnswerTitleArea } from '../../components/AnswerTitleArea';
+
 export const SurveyPreview = () => {
-  return <div>survey-preview</div>;
+  return (
+    <div
+      css={css`
+        height: auto;
+        display: flex;
+        justify-content: center;
+        padding: 50px 0;
+      `}
+    >
+      <div
+        css={css`
+          width: 770px;
+          display: flex;
+          flex-direction: column;
+          @media (max-width: 771px) {
+            width: 100%;
+          }
+        `}
+      >
+        <AnswerTitleArea />
+        <AnswerArea />
+      </div>
+    </div>
+  );
 };
